1e9e2b6065cd50a4e9f2d760e6e9484d0b485b73
Jarret Nachtigal <jarretn1224@gmail.com>
2021-11-11 14:45:12 -0500

    adds postindex page

---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="postsIndex">
+    <h1>{{ message }}</h1>
+    <div>
+      <div v-for="post in posts" :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <p>from user: {{ post.user_id }}</p>
+        <p>{{ post.body }}</p>
+        <img :src="post.image" :alt="post.title" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "All Posts",
+      posts: [],
+    };
+  },
+  created: function () {
+    this.showPosts();
+  },
+  methods: {
+    showPosts: function () {
+      axios.get("/posts").then((response) => {
+        this.posts = response.data;
+        console.log(response);
+      });
+    },
+  },
+};
+</script>
