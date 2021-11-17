e8e30609e006bcf1b7c82fcd02ab77eb28931533
Jarret Nachtigal <jarretn1224@gmail.com>
2021-11-17 15:01:26 -0500

    frontend validation / v-if for text fields

--- src/views/PostsNew.vue
+++ src/views/PostsNew.vue
@@ -8,10 +8,29 @@
       <div>
         <label>Title:</label>
         <input type="text" v-model="newPostParams.title" />
+        <!-- <input type="text" class="form-control is-invalid" v-model="newPostParams.title" /> -->
+        <small v-if="newPostParams.title.length < 6 && newPostParams.title.length > 0" class="text-danger">
+          title must be at least 6 characters
+        </small>
+        <small v-if="newPostParams.title.length > 6 && newPostParams.title.length < 20">
+          {{ 20 - this.newPostParams.title.length }} characters remaining
+        </small>
+        <small v-if="newPostParams.title.length > 6 && newPostParams.title.length > 20" class="text-danger">
+          title must be less than 20 characters
+        </small>
       </div>
       <div>
         <label>Body:</label>
         <input type="text" v-model="newPostParams.body" />
+        <small v-if="newPostParams.body.length < 6 && newPostParams.body.length > 0" class="text-danger">
+          body must be at least 6 characters
+        </small>
+        <small v-if="newPostParams.body.length > 6 && newPostParams.body.length < 300">
+          {{ 300 - this.newPostParams.body.length }} characters remaining
+        </small>
+        <small v-if="newPostParams.body.length > 300" class="text-danger">
+          body must be at less than 300 characters
+        </small>
       </div>
       <div>
         <label>Image Url:</label>
@@ -31,7 +50,7 @@ export default {
   data: function () {
     return {
       message: "Create a New Post",
-      newPostParams: {},
+      newPostParams: { title: "", body: "", image: "" },
       errors: [],
     };
   },
