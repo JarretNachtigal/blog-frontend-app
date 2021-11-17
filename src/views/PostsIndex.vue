4f8e3873eff8884df54da045f4766e8637f58667
Jarret Nachtigal <jarretn1224@gmail.com>
2021-11-17 13:57:20 -0500

    PostsIndex style

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -4,9 +4,10 @@
     <div>
       <div v-for="post in posts" :key="post.id">
         <div class="row">
-          <div class="col-12">
+          <div class="col-3"></div>
+          <div class="col-6">
             <div class="card">
-              <div class="card-body">
+              <div class="card-body" v-on:click="currentPost = post" v-bind:class="{ selected: post === currentPost }">
                 <h5 class="card-title">{{ post.title }}</h5>
                 <p class="card-text">from user: {{ post.user_id }}</p>
                 <p class="card-text">{{ post.body }}</p>
@@ -29,6 +30,12 @@ img {
   margin-right: auto;
   width: 40%;
 }
+h1 {
+  text-align: center;
+}
+.selected {
+  background-color: red;
+}
 </style>
 
 <script>
@@ -39,6 +46,8 @@ export default {
     return {
       message: "All Posts",
       posts: [],
+      currentPost: {},
+      // highlighted: false,
     };
   },
   created: function () {
